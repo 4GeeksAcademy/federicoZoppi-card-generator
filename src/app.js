7b14fa9c19fbcd4d49a847bf41f05bf047b52491
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-05-08 22:08:47 +0000

    federicoZoppi Generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generadorDeCarta = () => {
+  const suits = ["♦️", "♥️", "♣️", "♠️"];
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+ 
+  const suitElements = document.getElementsByClassName("suit");
+  const numberElement = document.querySelector(".number");
+
+
+  for (let suit of suitElements) {
+    suit.innerText = randomSuit;
+  }
+  numberElement.innerText = randomNumber;
+};
+
+window.onload = () => {
+  generadorDeCarta(); 
+
+  document.getElementById("generate-card").addEventListener("click", generadorDeCarta);
 };
